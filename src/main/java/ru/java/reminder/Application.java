64d70fb24dcc1d3bc6466870be3de5833d04f96c
v.mamedov <mamedov@banki.ru>
2024-10-20 00:25:19 +0300

    ELC Stack

--- src/main/java/ru/java/reminder/Application.java
+++ src/main/java/ru/java/reminder/Application.java
@@ -1,11 +1,14 @@
 package ru.java.reminder;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class Application {
     public static void main(final String[] args) {
+        log.info("Application started");
         SpringApplication.run(Application.class, args);
     }
 }
